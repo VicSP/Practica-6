e12f0a4c91bfb754f3d73140dcedd3623eeacad1
VicSP <74336881+VicSP@users.noreply.github.com>
2020-11-12 00:06:18 -0600

    Add files via upload

---
+++ POOP6/src/poop6/POOP6.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poop6;
+
+/**
+ *
+ * @author chino
+ */
+public class POOP6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Yo soy la clase POOP6");
+    }
+    
+}
